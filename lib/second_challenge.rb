480e3a203c4961b01f95e2b41892893a8d22ea39
wei feng <theweifeng@gmail.com>
2019-04-13 00:45:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends []
+      :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {},
